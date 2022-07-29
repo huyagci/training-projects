7b0b980c3d81975b411c38f5130e44361b3eb0ef
Hasan Umut Yağcı <hasanumutyagci@gmail.com>
2022-07-29 05:23:35 +0300

    Refactor empty lines on training-2.sql

--- database/sql/training-2.sql
+++ database/sql/training-2.sql
@@ -5,24 +5,16 @@ Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden ger�
 */
 
 -- 1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
-
 SELECT * from film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 -- Total rows: 236
 
 -- 2. .actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
-
 SELECT first_name, last_name from actor
 WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 -- Total rows: 10
 
 -- 3. film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
-
 SELECT * from film
 WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
 -- Total rows: 133
-
-
-
-
-
